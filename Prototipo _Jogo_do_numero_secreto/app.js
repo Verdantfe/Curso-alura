3dc431159842d3ba486168638ef4785ab8060f47
Verdantfe <161587253+Verdantfe@users.noreply.github.com>
2024-08-27 19:25:33 -0300

    Update app.js

--- Prototipo _Jogo_do_numero_secreto/app.js
+++ Prototipo _Jogo_do_numero_secreto/app.js
@@ -1,18 +1,13 @@
+//Aqui esta o codigo pra fazer um mini jogo com JS
 alert("Boas vindas ao jogo do número secreto");
 let numeroMaximo = 100;
-let numeroSecreto =  cost = parseInt(Math.random() * numeroMaximo + 1); //parseInt pega o numero INTEIRO, sem casas decimais
+let numeroSecreto =  cost = parseInt(Math.random() * numeroMaximo + 1); 
    console.log(numeroSecreto)
 let chute  
 let tentativas = 1;
-
-// usar sempre o live server para salvar no html(link do site)
-
-
 // enquanto chute não for igual ao n.s
 while (chute != numeroSecreto) {
-    chute = prompt(`Escolha um numero entre 1 a ${numeroMaximo}`);
-    
-    
+    chute = prompt(`Escolha um numero entre 1 a ${numeroMaximo}`);  
 // se o chute for igual ao numero secreto
     if (chute == numeroSecreto) {
         break;
@@ -22,17 +17,8 @@ while (chute != numeroSecreto) {
         } else {
         alert(`O numero secreto é maior que o ${chute}`);
     }
-    tentativas++; // isso serve para a variavel "tentativas" somar mais a cada ciclo, no caso esta dentro da função while
+    tentativas++; 
 }
 }
-
 let palavraTentatvia = tentativas > 1 ? "tentativas" : "tentativa"
 alert(`Isso aí! Você acertou número secreto ${numeroSecreto} em ${tentativas} ${palavraTentatvia}.`);
-
-
-//if (tentativas > 1) {
-//    alert(`Isso aí! Você acertou número secreto ${numeroSecreto} em ${tentativas} tentativas`);
-//}
-//    else {
-//        alert(`Isso aí! Você acertou número secreto ${numeroSecreto} em ${tentativas} tentativa`);
-//    }
